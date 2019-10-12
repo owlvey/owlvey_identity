b556024c9ca9d722bcd29ab03a6df5e69d8ac941
chenton147 <chenton147@hotmail.com>
2019-10-12 20:48:56 +0000

    .

--- scripts/run_server.bat
+++ scripts/run_server.bat
@@ -1,2 +1,2 @@
-dotnet build ./../Owlvey.Falcon.sln -v q
-dotnet run --project ./../src/Owlvey.Falcon.API/Owlvey.Falcon.API.csproj  --urls=http://0.0.0.0:5001/ --configuration Release
+dotnet build ./../Owlvey.Falcon.Authority.sln -v q
+dotnet run --project ./../src/Owlvey.Falcon.Authority.Presentation/Owlvey.Falcon.Authority.Presentation.csproj  --urls=http://0.0.0.0:5000/ --configuration Release
