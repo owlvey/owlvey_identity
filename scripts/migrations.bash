58d3f70b7443048e1fe6a5beaf8aea59d61bddf0
Gregory Valderrama <gcvalderrama@hotmail.com>
2019-10-16 19:05:54 -0500

    add

--- scripts/migrations.bash
+++ scripts/migrations.bash
@@ -4,8 +4,10 @@ rm ./../src/Owlvey.Falcon.Authority.Presentation/FalconAuthDb.db
 
 pushd ./../src/Owlvey.Falcon.Authority.Infra.Data.Sqlite
 
-dotnet ef migrations add ConfigurationDb -c PersistedGrantDbContext --project ./../src/Owlvey.Falcon.Authority.Infra.Data.Sqlite/Owlvey.Falcon.Authority.Infra.Data.Sqlite.csproj -o Migrations/ConfigurationDb
-dotnet ef migrations add PersistedGrantDb -c ConfigurationDbContext --project ./../src/Owlvey.Falcon.Authority.Infra.Data.Sqlite/Owlvey.Falcon.Authority.Infra.Data.Sqlite.csproj -o Migrations/PersistedGrantDb
-dotnet ef migrations add Initial -c FalconAuthDbContext --project ./../src/Owlvey.Falcon.Authority.Infra.Data.Sqlite/Owlvey.Falcon.Authority.Infra.Data.Sqlite.csproj
+dotnet ef migrations add ConfigurationDb -c PersistedGrantDbContext  -o Migrations/ConfigurationDb
+dotnet ef migrations add PersistedGrantDb -c ConfigurationDbContext -o Migrations/PersistedGrantDb
+dotnet ef migrations add Initial -c FalconAuthDbContext
 
 popd
+
+
--- src/Owlvey.Falcon.Authority.Infra.Data.Sqlite/Migrations/PersistedGrantDb/20191014054523_PersistedGrantDb.Designer.cs
+++ src/Owlvey.Falcon.Authority.Infra.Data.Sqlite/Migrations/PersistedGrantDb/20191014054523_PersistedGrantDb.Designer.cs
@@ -9,7 +9,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Owlvey.Falcon.Authority.Infra.Data.Sqlite.Migrations.PersistedGrantDb
 {
     [DbContext(typeof(ConfigurationDbContext))]
-    [Migration("20191012225648_PersistedGrantDb")]
+    [Migration("20191014054523_PersistedGrantDb")]
     partial class PersistedGrantDb
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
