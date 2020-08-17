90efb00b157df197d16c21e36ab11e7b54a1b1b1
gregory <gcvalderrama@hotmail.com>
2020-08-16 21:08:16 -0500

    add

--- src/Owlvey.Falcon.Authority.Presentation/Controllers/ApankuraController.cs
+++ src/Owlvey.Falcon.Authority.Presentation/Controllers/ApankuraController.cs
@@ -2,12 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace Apankura.Api.Controllers
 {
     [ApiController]
+    [AllowAnonymous]
     [Route("apankura")]
     public class ApankuraController : ControllerBase
     {        
