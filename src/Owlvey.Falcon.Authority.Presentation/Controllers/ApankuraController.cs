6b1f0efb5f46a6c5dd271912c594b1bff53f6f34
gregory <gcvalderrama@hotmail.com>
2020-08-16 20:17:11 -0500

    add

---
+++ src/Owlvey.Falcon.Authority.Presentation/Controllers/ApankuraController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Apankura.Api.Controllers
+{
+    [ApiController]
+    [Route("apankura")]
+    public class ApankuraController : ControllerBase
+    {        
+
+        private readonly ILogger<ApankuraController> _logger;
+
+        public ApankuraController(ILogger<ApankuraController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("liveness")]        
+        public Dictionary<string,string> Get()
+        {
+            var date = DateTime.Now;            
+            var map = new Dictionary<string, string>();
+            map.Add("date", date.ToString("dd-MM-yyyy HH:mm:ss"));            
+            return map;            
+        }
+    }
+}
